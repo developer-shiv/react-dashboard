6470352b1df9619c10e76d4c693398e3d2b6b79e
developer shiv <shiv.gic01@gmail.com>
2021-10-19 13:14:49 +0530

    this first commit for project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[Item,setItem]=useState('');
+ function handleInput(e){
+       e.preventDefault();
+    alert('hello')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+       <form id="to-do-form">
+           <input type="text" placeholder="Enter the value" value={Item} onChange={handleInput}/>
+           <button type="submit">Add</button>
+       </form>
       </header>
     </div>
   );
